7ae359ec77b0738e8018ebb54923f9e4b84bac75
Alimzhan Kamiev <alimkamiev@gmail.com>
2025-11-29 12:45:35 +0600

    name

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -2,7 +2,7 @@ export default function Hello() {
   return (
     <div>
       <h1>Hello, React!</h1>
-      <p>My name is [Your Name], Group COMP 2081_CS</p>
+      <p>My name is Alimzhan Kamiev, Group COMP 2081_CS</p>
     </div>
   );
 }
